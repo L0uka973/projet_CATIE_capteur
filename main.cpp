8f73846f4bf2cb6d933122575ab8f865d9f9ef60
ADELAIDE Louka <Louka.Adelaide@bordeaux-inp.fr>
2024-10-16 10:31:29 +0200

    interruption

--- main.cpp
+++ main.cpp
@@ -25,18 +25,14 @@ int main()
 */
 
 
-
-/*#include "mbed.h"
+/*
+#include "mbed.h"
 InterruptIn mypin(BUTTON1); 
 DigitalOut myled(LED1);
-
-
 
 using namespace std::chrono;
 
 Timer t;
-
-
 
 
 void timer_on()
@@ -55,7 +51,6 @@ void timer_off()
  
 }
 
-
 int main()
 {
     mypin.rise(&timer_on);  // attach the address of the flip function to the rising edge
@@ -67,7 +62,33 @@ int main()
         printf("mypin has value : %d \n\r", mypin.read());
         printf("The time taken was %llu milliseconds\n", duration_cast<milliseconds>(t.elapsed_time()).count());
     }
-}*/
+}
+*/
+
+#include "mbed.h"
+InterruptIn mypin(BUTTON1); 
+DigitalOut myled(LED1);
+
+void flip()
+{
+    myled = mypin;
+}
+
+
+int main()
+{
+    mypin.rise(&flip);  // attach the address of the flip function to the rising edge
+    mypin.fall(&flip);
+
+    while (1) {          // wait around, interrupts will interrupt this!
+ 
+        ThisThread::sleep_for(250);
+        printf("mypin has value : %d \n\r", mypin.read());
+    }
+}
+
+
+/*
 #include "mbed.h"
 DigitalIn mypin(BUTTON1); 
 DigitalOut myled(LED1);
@@ -90,6 +111,7 @@ int main()
         ThisThread::sleep_for(250);
     }
 }
+*/
 
 
 /*
