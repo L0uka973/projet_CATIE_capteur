1362bbc01f03b877a04ee7c510ee52cf1d0cb4b6
L0uka973 <loukaadelaide973@gmail.com>
2024-12-14 14:12:42 +0100

    Exercice Interruption
    Rajout des commentaires

--- main.cpp
+++ main.cpp
@@ -1,30 +1,33 @@
 #include "mbed.h"
 
-DigitalIn mypin(BUTTON1);// Déclare une entrée digitale `mypin` reliée à un bouton `BUTTON1` que l'utilisateur doit appuyer.
+InterruptIn mypin(BUTTON1); // Déclare une entrée digitale avec interruptions,  elle va détecter les changements d'état du bouton et générer des interruptions.
 
 
-DigitalOut myled(LED1); // Déclare une sortie digitale `myled` reliée à une LED `LED1` qui doit s'allumer et s'éteindre.
 
+DigitalOut myled(LED1); // Déclare une sortie digitale `myled` reliée à la LED `LED1` .
+
+
+void flip() // Cette fonction est appelée lors des interruptions.
+{
+    myled = mypin;  // synchronisation de l'état de la LED avec l'état du bouton.
+   
+}
 
 int main()
 {
+    mypin.rise(&flip); // Attache la fonction `flip` à l'interruption générée lors d'un front montant du signal (quand le bouton passe de 0 à 1).
+
     
-    if (mypin.is_connected()) { // Vérifie si `mypin` est correctement initialisé et connecté à un pin.
-        printf("mypin is connected and initialized! \n\r"); // Affiche un message dans la console si `mypin` est connecté.
-        
-    }
-        
-    mypin.mode(PullNone);// Configure le mode d'entrée du bouton en indéterminé ( L'état de départ n'est pas forcé à 1 pull-up ou 0 pull-down) en forçant pas l'état d'entrée à 1 ou 0 
-   
-    while (1) {
-        
-        printf("mypin has value : %d \n\r", mypin.read()); // Lit l'état actuel du bouton (1 = pas pressé, 0 = pressé) et l'affiche dans la console.
+    mypin.fall(&flip);    // Attache  la fonction `flip` à l'interruption générée lors du front descendant du signal (quand le bouton passe de 1 à 0).
 
-        
-        myled = mypin;// Affecte l'état du bouton à la LED.
-        // Si le bouton est pressé (0), la LED s'éteint ; sinon, elle s'allume.
 
-        ThisThread::sleep_for(250); // Ajoute une temporisation de 250 ms.
+    while (1) {    // Boucle infinie pendaqnt laquelle le programme attend les interruptions.       
+       
+        
+        ThisThread::sleep_for(250);     // Introduit une temporisation de 250 ms.
+
+
+        printf("mypin has value : %d \n\r", mypin.read()); // Affiche l'état actuel du bouton dans la console.
 
     }
 }
