c9d6f10fc5e7ba52e346195619a03a0889daaa41
L0uka973 <loukaadelaide973@gmail.com>
2024-12-14 14:14:16 +0100

    Exercice changement fréquence
    Rajout des commentaires

--- main.cpp
+++ main.cpp
@@ -1,33 +1,44 @@
+
+/************** FREQUENCE *********************/
 #include "mbed.h"
 
-InterruptIn mypin(BUTTON1); // Déclare une entrée digitale avec interruptions,  elle va détecter les changements d'état du bouton et générer des interruptions.
+
+Ticker flipper;
+DigitalOut myled(LED1);
+InterruptIn mypin(BUTTON1);
 
 
+int freq = 5; // Variable globale pour stocker la fréquence (initialisée à 5)
 
-DigitalOut myled(LED1); // Déclare une sortie digitale `myled` reliée à la LED `LED1` .
 
-
-void flip() // Cette fonction est appelée lors des interruptions.
+void flip()
 {
-    myled = mypin;  // synchronisation de l'état de la LED avec l'état du bouton.
-   
+    myled = !myled;  // Inversion de l'état de la LED 
+}
+
+// Fonction appelée lorsqu'il y a un appui sur le bouton 
+void frequence()
+{
+    // Décrémente la temps à chaque appui sur le bouton
+    freq = freq - 1;
+
+    // Modifie la temps de la fonction flip en réattachant le ticker avec la nouvelle fréquence
+    flipper.attach(&flip, freq); 
 }
 
 int main()
 {
-    mypin.rise(&flip); // Attache la fonction `flip` à l'interruption générée lors d'un front montant du signal (quand le bouton passe de 0 à 1).
+    // Première attache la fonction flip pour être appelée périodiquement avec une fréquence initiale
+    flipper.attach(&flip, freq);  
 
-    
-    mypin.fall(&flip);    // Attache  la fonction `flip` à l'interruption générée lors du front descendant du signal (quand le bouton passe de 1 à 0).
-
-
-    while (1) {    // Boucle infinie pendaqnt laquelle le programme attend les interruptions.       
-       
-        
-        ThisThread::sleep_for(250);     // Introduit une temporisation de 250 ms.
+    // Interrutpion sur appuie du bouton pour décrementer la fréquence
+    mypin.rise(&frequence);
 
 
-        printf("mypin has value : %d \n\r", mypin.read()); // Affiche l'état actuel du bouton dans la console.
-
+    while (1) {
+        // Affichage la variable fréquence sur la console
+        printf("freq: %d \n\r", freq);
+        ThisThread::sleep_for(250);
     }
 }
+/*******************************************/
